599eac49c19e3083bb8e975b68ce541020b5e0cd
haeriS2 <56130369+haeriS2@users.noreply.github.com>
2019-10-04 17:54:40 +0900

    Update 8282.py

--- 8282.py
+++ 8282.py
@@ -1,7 +1,10 @@
 import discord
+import random
 import os
 
 app = discord.Client()
+BOTTOKEN = "mytk"
+token = "BOTTOKEN"
 
 @app.event
 async def on_ready():
@@ -59,6 +62,18 @@ async def on_message(message):
     if message.content == "@골라주기":
         await message.channel.send(random.choice(["1번 골라주기","2번 골라주기","3번 골라주기","4번 골라주기","5번 골라주기"]))
 
+    if message.content == "@원딜픽":
+        await message.channel.send(random.choice(["타라","트리비아","카인","드렉슬러","토마스","나이오비","웨슬리","앨리셔","클레어","마를렌","샬럿","윌라드","미쉘","린","빅터","까미유","피터","엘리","마틴","미아","드니스","루시","하랑","리사","릭","탄야","캐럴","멜빈","헬레나","론","시드니","엘프리데","티샤"]))
+
+    if message.content == "@근딜픽":
+        await message.channel.send(random.choice(["로라스","휴톤","루이스","레나","도일","시바포","스텔라","다이무스","이글","레이튼","카를로스","호타루","트릭시","히카르도","자네트","아이작","레베카","브루스","제레온","티엔","제이","벨저","리첼","제키엘","라이샌더","루드빅","디아나","클리브","에바","레오노르","테이","티모시"]))
+
+    if message.content == "@서폿픽":
+        await message.channel.send(random.choice(["토마스","앨리셔","샬럿","윌라드","미쉘","까미유","마틴","리사","릭","캐럴","시드니","티샤"]))
+
+    if message.content == "@탱커픽":
+        await message.channel.send(random.choice(["로라스","휴톤","루이스","레나","도일","토마스","시바포","스텔라","다이무스","이글","레이튼","린","카를로스","호타루","트릭시","히카르도","자네트","아이작","레베카","브루스","제레온","티엔","제이","벨저","리첼","릭","제키엘","루드빅","디아나","클리브","에바","레오노르","테이","티모시"]))
+
     if message.content == "@랜덤픽":
         await message.channel.send(random.choice(["?","로라스","휴톤","루이스","타라","트리비아","카인","레나","드렉슬러","도일","토마스","나이오비","시바포","웨슬리","스텔라","앨리셔","클레어","다이무스","이글","마를렌","샬럿","윌라드","레이튼","미쉘","린","빅터","카를로스","호타루","트릭시","히카르도","까미유","자네트","피터","아이작","레베카","엘리","마틴","브루스","미아","드니스","제레온","루시","티엔","하랑","제이","벨저","스톰쉐도우(내맘픽)","리첼","리사","릭","제키엘","탄야","캐럴","라이샌더","루드빅","멜빈","디아나","클리브","헬레나","에바","론","레오노르","시드니","테이","티모시","엘프리데","티샤"]))
 
@@ -74,5 +89,4 @@ async def on_message(message):
         embed.set_image(url="https://i.imgur.com/kZPAxqp.gif") #embed에 이미지 추가
         await message.channel.send(embed=embed) #embed전송
         
-token = os.environ["BOT_TOKEN"]
 app.run(token)
